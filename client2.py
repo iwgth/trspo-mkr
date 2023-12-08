23bb73c76865d75109e6e2dc309ce1bbe56ac672
iwgth <114057948+iwgth@users.noreply.github.com>
2023-12-08 19:42:40 +0200

    first commit

---
+++ client2.py
@@ -0,0 +1,45 @@
+import socket
+import numpy as np
+import time
+
+def communicate_with_server():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 12345))
+    n, m, l = np.random.randint(1000, 2000, size=3)
+    matrix_a = np.random.randint(1, 10, size=(n, m))
+    matrix_b = np.random.randint(1, 10, size=(m, l))
+
+    print(f"Generated matrix by client2:")
+    print("Matrix A:")
+    print(matrix_a)
+    print("Matrix B:")
+    print(matrix_b)
+
+    print("Sending matrix sizes to server")
+    client.send(f"{n},{m},{l}".encode())
+
+    print("Sending matrices to server")
+    client.send(matrix_a.tobytes())
+    client.send(matrix_b.tobytes())
+
+    result_data = b""
+    while True:
+        chunk = client.recv(4096)
+        if not chunk:
+            time.sleep(0.1)
+            break
+        result_data += chunk
+
+    if len(result_data) == 0:
+        print("Server closed")
+    else:
+        result_matrix = np.frombuffer(result_data, dtype=int).reshape(n, l)
+
+        print("Received result matrix:")
+        print(result_matrix)
+
+    client.close()
+    print("Connection closed")
+
+if __name__ == "__main__":
+    communicate_with_server()
---
+++ server.py
@@ -0,0 +1,58 @@
+import socket
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import numpy as np
+def handle_client(client_socket):
+    try:
+        matrix_sizes = client_socket.recv(1024).decode().split(',')
+        n, m, l = map(int, matrix_sizes)
+
+        print(f"From server: Received matrix sizes: {n} x {m} and {m} x {l}")
+
+        if n <= 0 or m <= 0 or l <= 0:
+            raise ValueError("Matrix dimensions must be positive integers")
+
+        matrix_a_data = client_socket.recv(n * m * 4)  # Assuming int32 for simplicity
+        matrix_b_data = client_socket.recv(m * l * 4)
+
+        if len(matrix_a_data) != n * m * 4 or len(matrix_b_data) != m * l * 4:
+            raise ValueError("Invalid matrix data received")
+
+        matrix_a = np.frombuffer(matrix_a_data, dtype=int).reshape(n, m)
+        matrix_b = np.frombuffer(matrix_b_data, dtype=int).reshape(m, l)
+
+        print("From server: Received matrices:")
+        print("Matrix A:")
+        print(matrix_a)
+        print("Matrix B:")
+        print(matrix_b)
+
+        # Perform matrix multiplication in a separate thread
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            result_matrix = executor.submit(np.dot, matrix_a, matrix_b).result()
+
+        print("Sending result matrix to client")
+        client_socket.send(result_matrix.tobytes())
+    except ValueError as ve:
+        print(f"From server: Error: {ve}")
+    except Exception as e:
+        print(f"From server: Error: {e}")
+    finally:
+        client_socket.close()
+        print("From server: Connection closed")
+
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 12345))
+    server.listen(5)
+    print("Server listening on port 12345")
+
+    while True:
+        client, addr = server.accept()
+        print(f"From server: accepted connection from {addr}")
+        threading.Thread(target=handle_client, args=(client,)).start()
+
+
+if __name__ == "__main__":
+    start_server()
